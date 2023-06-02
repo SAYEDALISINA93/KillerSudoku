29cad534973052109d380eea67e58a2a1e5dbf47
Alisina <sayedalisinaa@gmail.com>
2023-06-02 09:43:44 +0200

    Initial Commit

---
+++ KillerSudoku/KillerSudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KillerSudokuApp.swift
+//  KillerSudoku
+//
+//  Created by Alisina on 02.06.23.
+//
+
+import SwiftUI
+
+@main
+struct KillerSudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
